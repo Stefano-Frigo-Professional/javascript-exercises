6ab504cbe9c143e1850502b7f044100b89894527
Stefano <sfrigouk@gmail.com>
2022-06-18 02:21:37 +0100

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let cel = (fah-32)/1.8;
+  if(!Number.isInteger(cel)){
+    return Number(cel.toFixed(1));
+  }
+  return cel;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let fah = (1.8 * cel) + 32;
+  if(!Number.isInteger(fah)){
+    return Number(fah.toFixed(1));
+  }
+  return fah;
 };
 
 // Do not edit below this line
