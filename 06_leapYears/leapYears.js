1cc7734ee0dd45c26d8a49c29f7fcbe47daa5aca
Stefano <sfrigouk@gmail.com>
2022-06-18 02:14:22 +0100

    Complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(!Number.isInteger(year) || year < 0)
+        return "ERROR";
 
+    let div4 = false;
+    let div100 = false;
+    let div400 = false;
+
+    if(year % 4 == 0)
+        div4 = true;
+    if(year%100 == 0)
+        div100 = true;
+    if(year%400 == 0)
+        div400 = true;
+
+    if(div4 && !div100)
+        return true;
+    else if (div4 && div100 && div400)
+        return true;
+    return false;
 };
 
 // Do not edit below this line
