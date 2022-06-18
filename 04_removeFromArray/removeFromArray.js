fd4a7f00b40978ce80daf9c5ac839e0fe33f2b79
Stefano <sfrigouk@gmail.com>
2022-06-18 02:04:04 +0100

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inpArray, ...removeArgs) {
+    const args = Array.from(removeArgs);
 
+    for(let i = 0; i<args.length; i++){
+        const index = inpArray.indexOf(args[i]);
+        if(index>-1)
+            inpArray.splice(index, 1);
+    }
+
+    return inpArray;
 };
 
 // Do not edit below this line
