0d45f32533503c51458e2f02769fd6691b8ec933
Stefano <sfrigouk@gmail.com>
2022-06-22 18:45:39 +0100

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum = 0;
+  a.forEach(num => {
+    sum+=num;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let total = 1;
+  a.forEach(num => {
+    total*=num;
+  });
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for(let i = 1; i<=a; i++){
+    fact*=i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
