98e2edae576044b7fb698f0d0a15a2463cdf5ff1
Stefano <sfrigouk@gmail.com>
2022-06-22 19:18:58 +0100

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n<1){
+        return "OOPS";
+    }
 
+    let fib = [0,1];
+    for(let i = 1; i < n; i++){
+        fib.push(fib[i-1] + fib[i]);
+    }
+
+    return fib[fib.length-1];
 };
 
 // Do not edit below this line
