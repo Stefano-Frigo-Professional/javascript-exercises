33bdeef8b7fe7d740b150436d2fa1d2bb12c9292
Stefano <sfrigouk@gmail.com>
2022-06-22 19:12:30 +0100

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    let rev = "";
+    const alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
 
+    let arrA = Array.from(a.toLowerCase());
+    arrA = arrA.filter((c) => {
+        return alphabet.includes(c);
+    });
+
+    for(let i = arrA.length-1; i>=0; i--){
+        rev+=arrA[i];
+    }
+
+    console.log(rev);
+    console.log(arrA.toString());
+
+    return rev === arrA.join('');
 };
+
+palindromes("Hello");
 
 // Do not edit below this line
 module.exports = palindromes;
